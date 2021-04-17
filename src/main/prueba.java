574e873a72b80b69ac8a1199ef9abe095007fe60
Juan Diego Cardona <juan.diego.cardona@accenture.com>
2021-04-17 18:47:56 -0500

    Update metodos class test

--- src/main/prueba.java
+++ src/main/prueba.java
@@ -1,10 +1,15 @@
 package main;
 
 public class prueba {
+    String test = "test";
+
+    String test2 = "test2";
 
     public String getTest() {
         return test;
     }
 
-    String test = "test";
+    public String getTest2() {
+        return test2;
+    }
 }
